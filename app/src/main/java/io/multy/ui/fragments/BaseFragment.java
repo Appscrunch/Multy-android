ef408233673443c1c8a6a3dcd93f5fe02abebb13
Andrew Frost <andreygrowl@gmail.com>
2017-12-12 09:11:54 +0300

    add mechanism to show errors, critical errors or progress bars

--- app/src/main/java/io/multy/ui/fragments/BaseFragment.java
+++ app/src/main/java/io/multy/ui/fragments/BaseFragment.java
@@ -11,6 +11,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.View;
 
+import io.multy.R;
+import io.multy.ui.fragments.dialogs.SimpleDialogFragment;
 import io.multy.viewmodels.BaseViewModel;
 
 public class BaseFragment extends Fragment {
@@ -20,6 +22,36 @@ public class BaseFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        subscribeToErrors();
+    }
+
+    protected BaseViewModel getBaseViewModel() {
+        return baseViewModel;
+    }
+
+    protected void setBaseViewModel(BaseViewModel baseViewModel) {
+        this.baseViewModel = baseViewModel;
+    }
+
+    protected void subscribeToErrors() {
+        if (baseViewModel != null) {
+            baseViewModel.errorMessage.observe(this, s ->
+                    SimpleDialogFragment.newInstanceNegative(getString(R.string.error), baseViewModel.errorMessage.getValue(),
+                            null).show(getFragmentManager(), "")
+            );
+
+            baseViewModel.isLoading.observe(this, aBoolean -> {
+                if (aBoolean) {
+                    //TODO show loading
+                } else {
+                    //TODO hide loading
+                }
+            });
+
+            baseViewModel.criticalMessage.observe(this, s -> {
+                //TODO show critical message and maybe exit?
+            });
+        }
     }
 
 }
