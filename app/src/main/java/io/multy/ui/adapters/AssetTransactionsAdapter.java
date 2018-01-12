0749b7a42e5df33fd3d1e90737a3bf2ee7df2a6d
Ihar Paliashchuk <ihar.paliashchuk@gmail.com>
2018-01-12 16:27:28 +0300

    add showing transactions
    # Conflicts:
    #	app/src/main/java/io/multy/ui/fragments/asset/AssetInfoFragment.java
    #	app/src/main/java/io/multy/viewmodels/WalletViewModel.java

--- app/src/main/java/io/multy/ui/adapters/AssetTransactionsAdapter.java
+++ app/src/main/java/io/multy/ui/adapters/AssetTransactionsAdapter.java
@@ -1,6 +1,8 @@
 package io.multy.ui.adapters;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,20 +10,30 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.multy.R;
+import io.multy.model.DataManager;
 import io.multy.model.entities.TransactionHistory;
+import io.multy.model.entities.wallet.CurrencyCode;
+import io.multy.ui.activities.AssetActivity;
+import io.multy.util.Constants;
+import io.multy.util.CryptoFormatUtils;
+import io.multy.util.NativeDataHelper;
+import timber.log.Timber;
 
 public class AssetTransactionsAdapter extends RecyclerView.Adapter<AssetTransactionsAdapter.Holder> {
 
-    private List<TransactionHistory> data;
+    private List<TransactionHistory> transactions;
 
-    public AssetTransactionsAdapter(List<TransactionHistory> data) {
-        this.data = data;
+    public AssetTransactionsAdapter() {
+        transactions = new ArrayList<>();
     }
 
     @Override
@@ -32,16 +44,94 @@ public class AssetTransactionsAdapter extends RecyclerView.Adapter<AssetTransact
     @Override
     public void onBindViewHolder(Holder holder, int position) {
         DecimalFormat fiatFormat = new DecimalFormat("#.##");
-        TransactionHistory transactionHistory = data.get(position);
-        ArrayList<TransactionHistory.StockExchangeRate> stockRates = transactionHistory.getStockExchangeRates();
-        String amountBtc = transactionHistory.getTxOutAmount();
-        double amountFiat = stockRates != null && stockRates.size() > 0 ? stockRates.get(0).getBtcUsd() : 16.000;
-        String amountFiatString = fiatFormat.format(amountFiat) + " USD";
+        TransactionHistory transaction = transactions.get(position);
+        boolean isInput = true;
+        switch (transaction.getTxStatus()) {
+            case Constants.TX_STATUS_IN_MEMPOOL_INCOMING:
+                isInput = true;
+                break;
+            case Constants.TX_STATUS_IN_BLOCK_INCOMING:
+                isInput = true;
+                break;
+            case Constants.TX_STATUS_IN_MEMPOOL_OUTCOMING:
+                isInput = false;
+                break;
+            case Constants.TX_STATUS_IN_BLOCK_OUTCOMING:
+                isInput = false;
+                break;
+            case Constants.TX_STATUS_IN_BLOCK_CONFIRMED:
+                isInput = true;
+                break;
+            case Constants.TX_STATUS_REJECTED:
+                isInput = false;
+                break;
+        }
+        String addressFirst;
+        String addressSecond;
+        if (isInput) {
+            if (transaction.getInputs().size() > Constants.ZERO) {
+                addressFirst = transaction.getInputs().get(Constants.ZERO).getAddress();
+                addressFirst = addressFirst.substring(Constants.ZERO, 6).concat(Constants.BULLETS_FIVE).concat(addressFirst.substring(addressFirst.length() - 6, addressFirst.length()));
+                holder.address.setText(addressFirst);
+                if (transaction.getInputs().size() > Constants.ONE) {
+                    addressSecond = transaction.getInputs().get(transaction.getInputs().size() - 1).getAddress();
+                    addressSecond = addressSecond.substring(Constants.ZERO, 6).concat(Constants.BULLETS_FIVE).concat(addressSecond.substring(addressSecond.length() - 6, addressSecond.length()));
+                    holder.address.append("\n");
+                    holder.address.append(addressSecond);
+                }
+            } else {
+                Timber.e("There are no inputs inside %s transaction", transaction.getTxId());
+            }
+            holder.operationImage.setImageDrawable(holder.operationImage.getContext().getDrawable(R.drawable.ic_recieve));
+        } else {
+            if (transaction.getOutputs().size() > Constants.ZERO) {
+                addressFirst = transaction.getOutputs().get(Constants.ZERO).getAddress();
+                if (!addressFirst.equals(transaction.getAddress())) {
+                    addressFirst = addressFirst.substring(Constants.ZERO, 6).concat(Constants.BULLETS_FIVE).concat(addressFirst.substring(addressFirst.length() - 6, addressFirst.length()));
+                    holder.address.setText(addressFirst);
+                }
+                if (transaction.getOutputs().size() > Constants.ONE) {
+                    addressSecond = transaction.getOutputs().get(transaction.getOutputs().size() - 1).getAddress();
+                    if (!addressSecond.equals(transaction.getAddress())) {
+                        addressSecond = addressSecond.substring(Constants.ZERO, 6).concat(Constants.BULLETS_FIVE).concat(addressSecond.substring(addressSecond.length() - 6, addressSecond.length()));
+                        if (!TextUtils.isEmpty(holder.address.getText())) {
+                            holder.address.append("\n");
+                        }
+                        holder.address.append(addressSecond);
+                    }
+                }
+            } else {
+                Timber.e("There are no inputs inside %s transaction", transaction.getTxId());
+            }
+            holder.operationImage.setImageDrawable(holder.operationImage.getContext().getDrawable(R.drawable.ic_send));
+        }
+
+        double amount = Double.parseDouble(transaction.getTxOutAmount()) / Math.pow(10, 8);
+
+        holder.amount.setText(String.valueOf(amount));
+        ArrayList<TransactionHistory.StockExchangeRate> stockRates = transaction.getStockExchangeRates();
+        double fiatExchangePrice = stockRates != null && stockRates.size() > 0 ? stockRates.get(0).getBtcUsd() : 16000;
+        String amountFiatString = fiatFormat.format(fiatExchangePrice * amount).concat(Constants.SPACE).concat(CurrencyCode.USD.name());
+        holder.fiat.setText(amountFiatString);
+//        holder.comment.setText(transaction.);
+
+        Date date = new Date(transaction.getBlockTime());
+        SimpleDateFormat formatter = new SimpleDateFormat("dd MMMM yyyy HH:mm", Locale.ENGLISH);
+        holder.time.setText(formatter.format(transaction.getBlockTime() * 1000));
+//        formatter = new SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH);
+//        Timber.e("timestamp %s", transaction.getBlockTime());
+//        Timber.e("date %s", formatter.format(date.getTime() * 1000));
+//        holder.date.setText(formatter.format(transaction.getBlockTime() * 1000));
     }
 
     @Override
     public int getItemCount() {
-        return data.size();
+        return transactions.size();
+    }
+
+    public void setTransactions(List<TransactionHistory> transactions) {
+        this.transactions = transactions;
+        notifyDataSetChanged();
     }
 
     class Holder extends RecyclerView.ViewHolder {
@@ -52,17 +142,17 @@ public class AssetTransactionsAdapter extends RecyclerView.Adapter<AssetTransact
         @BindView(R.id.image_operation)
         ImageView operationImage;
 
-        @BindView(R.id.text_name)
-        TextView name;
+        @BindView(R.id.text_address)
+        TextView address;
 
-        @BindView(R.id.text_description)
-        TextView description;
+        @BindView(R.id.text_time)
+        TextView time;
 
         @BindView(R.id.text_amount)
         TextView amount;
 
-        @BindView(R.id.text_time)
-        TextView time;
+        @BindView(R.id.text_fiat)
+        TextView fiat;
 
         @BindView(R.id.text_comment)
         TextView comment;
