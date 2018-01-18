89b9626fc8b8bd3dce4f58326c78b816c91af344
Anschutz1927 <anschutz1927@gmail.com>
2018-01-18 10:58:41 +0300

    fix Multy.java exception

--- app/src/main/java/io/multy/Multy.java
+++ app/src/main/java/io/multy/Multy.java
@@ -7,8 +7,6 @@ import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.util.Base64;
 
 import com.samwolfand.oneprefs.Prefs;
@@ -17,7 +15,6 @@ import com.tozny.crypto.android.AesCbcWithIntegrity;
 import net.khirr.library.foreground.Foreground;
 
 import java.security.GeneralSecurityException;
-import java.security.NoSuchAlgorithmException;
 
 import io.branch.referral.Branch;
 import io.multy.storage.RealmManager;
@@ -51,15 +48,15 @@ public class Multy extends Application {
 
         context = getApplicationContext();
 
-        if (Prefs.getString(Constants.PREF_IV, "").equals("")) {
-            try {
-                byte[] iv = AesCbcWithIntegrity.generateIv();
-                String vector = new String(Base64.encode(iv, Base64.NO_WRAP));
-                Prefs.putString(Constants.PREF_IV, vector);
-            } catch (GeneralSecurityException e) {
-                e.printStackTrace();
-            }
-        }
+//        if (Prefs.getString(Constants.PREF_IV, "").equals("")) {
+//            try {
+//                byte[] iv = AesCbcWithIntegrity.generateIv();
+//                String vector = new String(Base64.encode(iv, Base64.NO_WRAP));
+//                Prefs.putString(Constants.PREF_IV, vector);
+//            } catch (GeneralSecurityException e) {
+//                e.printStackTrace();
+//            }
+//        }
 
         if (Prefs.contains(Constants.PREF_VERSION)) {
             PackageInfo pInfo = null;
@@ -72,10 +69,10 @@ public class Multy extends Application {
             }
         }
 
-        final String counter = SecurePreferencesHelper.getString(this, Constants.PIN_COUNTER);
-        if (counter.equals("")) {
-            SecurePreferencesHelper.putString(this, Constants.PIN_COUNTER, String.valueOf(6));
-        }
+//        final String counter = SecurePreferencesHelper.getString(this, Constants.PIN_COUNTER);
+//        if (counter.equals("")) {
+//            SecurePreferencesHelper.putString(this, Constants.PIN_COUNTER, String.valueOf(6));
+//        }
 
         Foreground.Companion.init(this);
     }
@@ -89,13 +86,16 @@ public class Multy extends Application {
      * Generates unique new key for our DATABASE and writes it to our secure encrypted preferences
      * only after generating key we can access the DB
      */
-    public static void makeInitialized() {
+    public static void makeInitialized(Activity activity) {
         Prefs.putBoolean(Constants.PREF_APP_INITIALIZED, true);
         try {
+            byte[] iv = AesCbcWithIntegrity.generateIv();
+            String vector = new String(Base64.encode(iv, Base64.NO_WRAP));
+            Prefs.putString(Constants.PREF_IV, vector);
             String key = new String(Base64.encode(EntropyProvider.generateKey(512), Base64.NO_WRAP));
-            SecurePreferencesHelper.putString(getContext(), Constants.PREF_KEY, key);
+            SecurePreferencesHelper.putString(activity, Constants.PREF_KEY, key);
             RealmManager.open(getContext());
-        } catch (NoSuchAlgorithmException e) {
+        } catch (GeneralSecurityException e) {
             e.printStackTrace();
         }
     }
@@ -104,7 +104,7 @@ public class Multy extends Application {
         try {
             RealmManager.removeDatabase(activity);
             Prefs.clear();
-            Multy.makeInitialized();
+            Multy.makeInitialized(activity);
             Realm.init(activity);
         } catch (Exception exc) {
 //            System.exit(0);
@@ -113,23 +113,5 @@ public class Multy extends Application {
 
         activity.startActivity(new Intent(activity, SplashActivity.class).setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY));
         activity.finish();
-    }
-
-    public static boolean isConnected(Context context) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
-
-        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
-            android.net.NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-            android.net.NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-
-            if ((mobile != null && mobile.isConnectedOrConnecting()) || (wifi != null && wifi.isConnectedOrConnecting())) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
     }
 }
