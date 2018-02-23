5f9fea6970df88f82229768fd95797a8185b3340
Ihar Paliashchuk <ihar.paliashchuk@gmail.com>
2018-02-23 13:43:14 +0300

    add copy, share and cancel to private key dialog

--- app/src/main/java/io/multy/ui/fragments/dialogs/PrivateKeyDialogFragment.java
+++ app/src/main/java/io/multy/ui/fragments/dialogs/PrivateKeyDialogFragment.java
@@ -8,14 +8,19 @@ package io.multy.ui.fragments.dialogs;
 
 import android.app.Dialog;
 import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
 import android.support.design.widget.BottomSheetDialogFragment;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.multy.Multy;
+import butterknife.OnClick;
 import io.multy.R;
 import io.multy.model.entities.wallet.WalletAddress;
 import io.multy.storage.RealmManager;
@@ -28,39 +33,67 @@ import io.multy.viewmodels.WalletViewModel;
 
 public class PrivateKeyDialogFragment extends BottomSheetDialogFragment {
 
-    @BindView(R.id.text_key)
-    TextView textKey;
-
-    private WalletAddress address;
-
     public static PrivateKeyDialogFragment getInstance(WalletAddress address) {
         PrivateKeyDialogFragment fragment = new PrivateKeyDialogFragment();
         fragment.setAddress(address);
         return fragment;
     }
 
+    @BindView(R.id.text_key)
+    TextView textKey;
+
+    private WalletAddress address;
+    private WalletViewModel viewModel;
+
     @Override
     public void setupDialog(Dialog dialog, int style) {
         View view = View.inflate(getContext(), R.layout.bottom_sheet_private_key, null);
         ButterKnife.bind(this, view);
         dialog.setContentView(view);
+
+        viewModel = ViewModelProviders.of(getActivity()).get(WalletViewModel.class);
+
+        if (!TextUtils.isEmpty(getPrivateKey())) {
+            textKey.setText(getPrivateKey());
+        } else {
+            dismiss();
+        }
+    }
+
+    @OnClick(R.id.button_copy)
+    public void onClickCopy() {
+        if (!TextUtils.isEmpty(getPrivateKey())) {
+            viewModel.copyToClipboard(getActivity(), getPrivateKey());
+        }
+    }
+
+    @OnClick(R.id.button_share)
+    public void onClickShare() {
+        if (!TextUtils.isEmpty(getPrivateKey())) {
+            viewModel.share(getActivity(), getPrivateKey());
+        }
+    }
+
+    @OnClick(R.id.button_cancel)
+    public void onClickCancel() {
+        dismiss();
+    }
+
+    private void setAddress(WalletAddress address) {
+        this.address = address;
+    }
+
+    private String getPrivateKey() {
         try {
-            WalletViewModel viewModel = ViewModelProviders.of(getActivity()).get(WalletViewModel.class);
             byte[] seed = RealmManager.getSettingsDao().getSeed().getSeed();
             int walletIndex = viewModel.getWalletLive().getValue().getWalletIndex();
             int addressIndex = address.getIndex();
             int currency = NativeDataHelper.Currency.BTC.getValue();
-            String privateKey = NativeDataHelper.getMyPrivateKey(seed, walletIndex, addressIndex, currency);
-            textKey.setText(privateKey);
-
+            return NativeDataHelper.getMyPrivateKey(seed, walletIndex, addressIndex, currency);
         } catch (Throwable throwable) {
             throwable.printStackTrace();
-            Toast.makeText(Multy.getContext(), "Error while build private key ;(",
-                    Toast.LENGTH_SHORT).show();
+            Toast.makeText(Multy.getContext(), "Error while build private key ;(", Toast.LENGTH_SHORT).show();
+            return null;
         }
-    }
-
-    public void setAddress(WalletAddress address) {
-        this.address = address;
     }
 }
